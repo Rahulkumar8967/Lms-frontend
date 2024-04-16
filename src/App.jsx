21b71c8648bc5dafc05053338c4422836a6c60b0
RahulKumar8967 <sainirahul8967@gmail.com>
2024-04-16 22:48:09 +0530

    Adding signup/login flow

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import HomePage from './Pages/HomePage';
 
 import AboutUs from './Pages/AboutUs';
 import NotFound from './Pages/NotFound';
+import Signup from './Pages/Signup';
 function App() {
   return (
     <Routes> 
       <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<AboutUs />} ></Route>
+      <Route path="/signup" element={<Signup />} />
       <Route path="*" element={<NotFound />}></Route>
     </Routes>
   );
